4f06f1f0ca575ec8ed2900a1a048be4b17df353f
Divyanshu Rai <raidivyanshu19032000@gmail.com>
2026-04-19 19:00:48 +0530

    Singup form completed.

--- src/app/components/signupcomponent/signupcomponent.ts
+++ src/app/components/signupcomponent/signupcomponent.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { first } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
+import ValidateForm from '../../helpers/ValidateForm';
 
 @Component({
   selector: 'app-signupcomponent',
@@ -11,18 +13,54 @@ import { first } from 'rxjs';
 })
 export class Signupcomponent {
 
-  signupform! : FormGroup;
-  constructor(private fb : FormBuilder, private router : Router){
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon: string = 'fa-eye-slash'
+
+  signupform!: FormGroup;
+  constructor(private fb: FormBuilder, private router: Router,
+    private authService: AuthService
+  ) {
 
   }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.signupform = this.fb.group({
-      firstName : ['', Validators.required], 
-      lastName : ['', Validators.required], 
-      email : ['', Validators.required], 
-      userName : ['', Validators.required], 
-      password : ['', Validators.required]
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
     })
+  }
+
+
+  hideShowPass() {
+    this.isText = !this.isText; // true 
+    this.isText ? (this.eyeIcon = 'fa-eye') : (this.eyeIcon = 'fa-eye-slash');
+    this.isText ? (this.type = 'text') : (this.type = 'password');
+  }
+
+  onSubmit() {
+    // const signupFormValue = this.signupform.value;
+    // console.log(signupFormValue);
+    if (this.signupform.valid) {
+      console.log('Save Data', this.signupform.value);
+      this.authService.signup(this.signupform.value).subscribe({
+        next: (res) => {
+          alert(res.message)
+          this.signupform.reset(); 
+          this.router.navigate(['login']);
+        },
+        error: (err) => {
+          alert(err?.error.message);
+        }
+      })
+    } else {
+      // alert('Form field is not valid!!');
+      console.log('Form is not valid');
+      ValidateForm.validateAllFormFields(this.signupform);
+    }
+
   }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+
+  private baseUrl: string = 'https://localhost:7255/api/User/';
+  // private userPayload : any;
+  constructor(private httpClient: HttpClient, private router: Router) {
+
+  }
+
+  signup(userObj: any) {
+    return this.httpClient.post<any>(`${this.baseUrl}register`, userObj);
+  }
+
+  login(loginObj: any) {
+    return this.httpClient.post<any>(`${this.baseUrl}register`, loginObj);
+  }
+
+}
