2de8da4cf36896651dff7c79c9e54e16077dd4f4
Divyanshu Rai <raidivyanshu19032000@gmail.com>
2026-04-19 17:43:17 +0530

    Singup form

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -6,10 +6,11 @@ import { App } from './app';
 import { Logincomponent } from './components/logincomponent/logincomponent';
 import { Signupcomponent } from './components/signupcomponent/signupcomponent';
 import { Resetcomponent } from './components/resetcomponent/resetcomponent';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [App,Logincomponent, Signupcomponent, Resetcomponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
--- src/app/components/signupcomponent/signupcomponent.ts
+++ src/app/components/signupcomponent/signupcomponent.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-signupcomponent',
@@ -6,4 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './signupcomponent.html',
   styleUrl: './signupcomponent.css',
 })
-export class Signupcomponent {}
+export class Signupcomponent {
+
+  signupform! : FormGroup;
+  constructor(private fb : FormBuilder, private router : Router){
+
+  }
+
+  ngOnInit(): void{
+    this.signupform = this.fb.group({
+      firstName : ['', Validators.required], 
+      lastName : ['', Validators.required], 
+      email : ['', Validators.required], 
+      userName : ['', Validators.required], 
+      password : ['', Validators.required]
+    })
+  }
+}
